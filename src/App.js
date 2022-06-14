2f19b1a32bafbe81befdff48a7cbc4575b87eb26
Rahul <rahulsharma57002@gmail.com>
2022-06-14 19:48:10 +0530

    error handling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
 
-class App extends React.Component() {
+class App extends React.Component {
 
-  constructor() {
+  constructor () {
     super();
     this.state = {
       products: [
